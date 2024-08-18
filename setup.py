dccfa4ccf2a7a0a1f01602452ca86dfb9e2b6217
Niklas Henning <niklashenningbusiness@gmail.com>
2024-08-18 21:51:32 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='pytablericons',
-    version='1.0.0',
+    version='1.0.1',
     author='Niklas Henning',
     author_email='business@niklashenning.com',
     license='MIT',
